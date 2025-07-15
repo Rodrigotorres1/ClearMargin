d2d0b990390d65f012a4be4533fc15a38ebf42dc
caiodvrodrigues <caiodvrodrigues@gmail.com>
2025-07-15 18:01:51 -0300

    Mensagem do commit

---
+++ home.js
@@ -0,0 +1,11 @@
+document.getElementById('btnLucro').addEventListener('click', function () {
+  window.location.href = 'calculadoralucro.html';
+});
+
+document.getElementById('btnInvestimento').addEventListener('click', function () {
+  window.location.href = 'simuladorinvestimento.html';
+});
+
+document.getElementById('btnCotacao').addEventListener('click', function () {
+  window.location.href = 'cotacaomoedas.html';
+});
